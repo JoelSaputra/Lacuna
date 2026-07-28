3fd0296b3e9ef126fb30aab83b478640b6e43cfc
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-28 19:35:41 -0400

    load_folder test success

--- core/load.py
+++ core/load.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 
 
+def load_folder(folder):
+    folder = Path(folder)
+    for file in sorted(folder.iterdir()):
+        print(file)
 
+
+
+
+if __name__ == "__main__":
+    load_folder('data/corpus')
+        
     
