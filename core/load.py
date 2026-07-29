aa27bf681e6528e95094030ac4897276e54e0a1d
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-28 20:52:19 -0400

    load_folder success

--- core/load.py
+++ core/load.py
@@ -2,12 +2,25 @@ from pathlib import Path
 
 
 def load_folder(folder):
+
     folder = Path(folder)
+    files = []
+    
     for file in sorted(folder.iterdir()):
-        print(file)
 
+        file_dict = {}
+        if(file.suffix != ".md"):
+            continue
+        text = file.read_text(encoding="utf-8")
+        
+        file_dict["id"] = file.stem
+        file_dict["source"] = file.name
+        file_dict["text"] = text
 
+        files.append(file_dict)
 
+    return files
+        
 
 if __name__ == "__main__":
     load_folder('data/corpus')
