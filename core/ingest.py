ba6bbcaebd90b306867f1af214a994c71d006199
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-07-30 21:57:26 -0400

    ingest.py test done

--- core/ingest.py
+++ core/ingest.py
@@ -1,10 +1,41 @@
 from pathlib import Path
-from load import load_folder
-from chunking import chunk
+from core.load import load_folder
+from core.chunking import chunk
+import chromadb
 
+
+client = chromadb.PersistentClient(path="storage/chroma")
 
 
 def ingest():
+    collection = client.get_or_create_collection("fiba_rules")
+    folder = load_folder("data/corpus")
+    texts = []
+    collection_id = []
+    collection_metadatas = []
+    
+
+    for file in folder:
+        for i, chunk_text in enumerate(chunk(file["text"])):
+            texts.append(chunk_text)
+            collection_id.append(f"{file['id']}-{i}")
+            collection_metadatas.append({"doc_id": file["id"]})
+
+
+    collection.add(
+            documents=texts,
+            ids=collection_id,
+            metadatas=collection_metadatas
+        )
+
+
+
+    
+
+    
+
+
+
 
 
 
