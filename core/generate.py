2b9aab6396c6701334661231cee17b0605378ec1
Joel Clenn Saputra <joel.clenn03@gmail.com>
2026-08-01 19:41:42 -0400

    generate fix update => context formatted

--- core/generate.py
+++ core/generate.py
@@ -1,5 +1,3 @@
-from pathlib import Path
-from core.ingest import ingest
 from core.retrieve import retrieve
 from config import *
 from google import genai
@@ -11,11 +9,12 @@ API_KEY = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=API_KEY)
 model = MODEL
 
-def generate_response(question: str):
-    retrieved_docs = retrieve(question)
+def generate_response(question: str, hits):
+
+    context = "\n\n".join(f"[{doc_id}]\n{text}" for doc_id, text in zip(hits["doc_id"], hits["documents"]))
 
     prompt = f"""Only answer this question: {question} 
-                 based on the following context: {retrieved_docs["documents"]}
+                 based on the following context: {context}
                  If the answer is not contained within the context provided, say "NOT ANSWERABLE" and do not make up an answer.
               """
     
